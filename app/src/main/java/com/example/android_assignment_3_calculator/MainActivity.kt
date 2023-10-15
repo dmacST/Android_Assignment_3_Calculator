905ce6bad048cb28fe364e7ac397f49fcf642cfd
dmacST <58571753+dmacST@users.noreply.github.com>
2023-10-15 14:38:49 +0400

    added functional

--- app/src/main/java/com/example/android_assignment_3_calculator/MainActivity.kt
+++ app/src/main/java/com/example/android_assignment_3_calculator/MainActivity.kt
@@ -1,11 +1,105 @@
 package com.example.android_assignment_3_calculator
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var displayTextView: TextView
+    private var currentInput = ""
+    private var result = 0.0
+    private var operator = ""
+    private var newInput = true
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        displayTextView = findViewById(R.id.solution)
+    }
+
+    fun onButtonClick(view: View) {
+        if (view is Button) {
+            val buttonText = view.text.toString()
+
+            if (newInput) {
+                currentInput = ""
+                newInput = false
+            }
+
+            if (buttonText == "." && currentInput.contains(".")) {
+                return
+            }
+
+            currentInput += buttonText
+            displayTextView.text = currentInput
+        }
+    }
+
+    fun onAddClick(view: View) {
+        performOperation()
+        operator = "+"
+    }
+
+    fun onSubtractClick(view: View) {
+        performOperation()
+        operator = "-"
+    }
+
+    fun onMultiplyClick(view: View) {
+        performOperation()
+        operator = "*"
+    }
+
+    fun onDivideClick(view: View) {
+        performOperation()
+        operator = "/"
+    }
+
+    fun onEqualsClick(view: View) {
+        performOperation()
+        operator = ""
+    }
+
+    fun onClearClick(view: View) {
+        currentInput = ""
+        result = 0.0
+        operator = ""
+        displayTextView.text = "0"
+        newInput = true
+    }
+
+    fun onPlusMinusClick(view: View) {
+        if (currentInput.isNotEmpty() && currentInput != "0") {
+            currentInput = if (currentInput.startsWith("-")) {
+                currentInput.substring(1)
+            } else {
+                "-$currentInput"
+            }
+            displayTextView.text = currentInput
+        }
+    }
+
+    fun Double.isInt() = this % 1 == 0.0
+
+    private fun performOperation() {
+        val input = currentInput.toDouble()
+        when (operator) {
+            "+" -> result += input
+            "-" -> result -= input
+            "*" -> result *= input
+            "/" -> result /= input
+            else -> result = input
+        }
+
+        if (result.isInt()) {
+            currentInput = result.toInt().toString()
+        } else {
+            currentInput = result.toString()
+        }
+
+        displayTextView.text = currentInput
+        newInput = true
     }
 }
